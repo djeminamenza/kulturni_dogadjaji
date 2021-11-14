ad266e3c5acd758661543b9ff8ebf93b3a2682f4
Gorica Aleksic <agorica95@gmail.com>
2021-11-14 22:29:50 +0100

    home view za admina

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,4 @@ Route::view('/registracija', 'registracija');
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('admin/home', [HomeController::class, 'adminHome'])->name('admin.home')->middleware('is_admin');
