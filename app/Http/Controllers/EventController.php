b101edda175a5202a6420254e72adeac2eb7e66c
Jelena_Stanic <jelenastn30@gmail.com>
2021-11-25 21:36:08 +0100

    select u dogadjajima

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -10,9 +10,21 @@ class EventController extends Controller
 
     public function showEvents()
     {
-        $events = Event::all();
+        $events = DB::table('events')
+        ->join('categories', 'events.id_category', '=', 'categories.id')
+        ->join('ages', 'events.id_years', '=', 'ages.id')
+        ->join('places', 'events.id_place', '=', 'places.id')
+        ->join('cities', 'events.id_city', '=', 'cities.id')
+        ->select('events.id', 'events.name', 'events.starting_date', 'events.ending_date', 'ages.ages', 
+        'categories.description as categ', 'cities.name', 'places.description')
+        ->get();
         return view('dogadjaji', compact('events'));
     }
 
 
 }
+
+           
+
+
+            
